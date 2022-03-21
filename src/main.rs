8a86e223d7bee3464d1a43107104d4685ca06bde
MacRat <m@crat.jp>
2022-03-21 13:30:57 +0900

    feat: implement interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
+use std::cell::RefCell;
+use std::io::Write;
 use std::rc::Rc;
 
 mod funcs;
 mod parser;
 mod scope;
 mod types;
+
+fn compute(scope: &Rc<RefCell<scope::Scope>>, parser: &mut parser::Parser) {
+    loop {
+        match parser.pop() {
+            Some(expr) => match expr.compute(Rc::clone(scope)) {
+                Ok(result) if result.is_nil() => println!(""),
+                Ok(result) => println!("> {}\n", result),
+                Err(err) => {
+                    println!("! {}\n", err);
+                    std::process::exit(1);
+                }
+            },
+            None => break,
+        }
+    }
+}
 
 fn main() {
     let scope = scope::Scope::new(None);
@@ -12,32 +30,40 @@ fn main() {
         std::process::exit(-1);
     }
 
-    let input = r"
-        (def f (func (x)
-          ((def loop (func (n)
-            (set x (* x n))
-            (if (= n 1)
-              x
-              (loop (- n 1)))))
-            (- x 1))))
+    let mut parser = parser::Parser::new();
 
-        (println (f 5))
-    ";
+    let stdin = std::io::stdin();
+    let mut input = String::new();
+    loop {
+        if parser.is_completed() {
+            print!("< ");
+        } else {
+            print!("| ");
+        }
+        let _ = std::io::stdout().flush();
 
-    match parser::parse(input) {
-        Ok(xs) => {
-            for expr in xs.iter() {
-                println!("< {}", expr);
-                match expr.compute(Rc::clone(&scope)) {
-                    Ok(result) if result.is_nil() => println!(""),
-                    Ok(result) => println!("> {}\n", result),
-                    Err(err) => println!("! {}\n", err),
+        input.clear();
+        match stdin.read_line(&mut input) {
+            Ok(0) => break,
+            Ok(_) => {
+                if let Err(err) = parser.feed(input.as_str()) {
+                    println!("! {}", err);
+                    std::process::exit(1);
                 }
+
+                compute(&scope, &mut parser);
+            }
+            Err(err) => {
+                println!("! {}", err);
+                std::process::exit(-1);
             }
         }
-        Err(err) => {
-            println!("{}", err);
-            std::process::exit(-1);
-        }
     }
+
+    if let Err(err) = parser.flush() {
+        println!("! {}", err);
+        std::process::exit(1);
+    }
+
+    compute(&scope, &mut parser);
 }
